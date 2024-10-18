b3b407d6847d8db5ee5fde490515c6ab56e2d6e9
Muhammad Kashif <mohammadkashif9732@gmail.com>
2024-10-18 12:54:59 +0500

    Initial Commit

---
+++ app/routes/notes.jsx
@@ -0,0 +1,36 @@
+import { redirect } from "@remix-run/node";
+import NewNote, {links as newNoteLinks} from "../components/NewNote";
+import { getStoredNotes, storeNotes } from "../data/notes";
+import NoteList, {links as noteListLinks} from "../components/NoteList";
+import { useLoaderData } from "@remix-run/react";
+
+export default function NotesPage() {
+    const notes = useLoaderData();
+    useLoaderData();
+    return(
+        <main>
+            <NewNote />
+            <NoteList notes={notes}/>
+        </main>
+    );
+}
+
+export async function loader() {
+    const notes = await getStoredNotes();
+    return notes;
+}
+
+export async function action({request}) {
+    const formData = await request.formData();
+    const noteData = Object.fromEntries(formData);
+    // add validation 
+    const existingNotes = await getStoredNotes();
+    noteData.id = new Date().toISOString();
+    const updatedNotes = existingNotes.concat(noteData);
+    await storeNotes(updatedNotes);
+    return redirect('/notes');
+}
+
+export function links() {
+    return [...newNoteLinks(), ...noteListLinks()];
+}
